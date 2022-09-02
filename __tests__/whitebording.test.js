ebc7f2b32bc482356dc20ebd87b4241596dbe792
zbl14 <ifthereisoneday@gmail.com>
2022-09-02 10:40:40 -0700

    arrDedupeWithSet(), all tests passed

--- __tests__/whitebording.test.js
+++ __tests__/whitebording.test.js
@@ -1,4 +1,8 @@
-import { stringToUrls, arrDedupe } from "../src/js/whitebording";
+import {
+  stringToUrls,
+  arrDedupe,
+  arrDedupeWithSet,
+} from "../src/js/whitebording";
 
 describe("stringToUrls", () => {
   test("should return empty string if the input is an empty string", () => {
@@ -37,6 +41,26 @@ describe("arrDedupe", () => {
     const input = [7, 9, "hi", 12, "hi", 7, 53];
     const result = arrDedupe(input);
     console.log(result);
+    expect(result).toEqual([7, 9, "hi", 12, 53]);
+  });
+});
+
+describe("arrDedupeWithSet", () => {
+  test("should return empty array if the input is an empty array", () => {
+    const input = [];
+    const result = arrDedupeWithSet(input);
+    expect(result).toEqual([]);
+  });
+
+  test("should return the array if the input array contain only one element", () => {
+    const input = [1];
+    const result = arrDedupeWithSet(input);
+    expect(result).toEqual([1]);
+  });
+
+  test("should return deduped array", () => {
+    const input = [7, 9, "hi", 12, "hi", 7, 53];
+    const result = arrDedupeWithSet(input);
     expect(result).toEqual([7, 9, "hi", 12, 53]);
   });
 });
--- src/js/whitebording.js
+++ src/js/whitebording.js
@@ -44,6 +44,12 @@ export const arrDedupe = (arr) => {
   return res;
 };
 
+export const arrDedupeWithSet = (arr) => {
+  if (arr.length <= 1) return arr;
+  const arrSet = new Set(arr);
+  return [...arrSet];
+};
+
 // Question #3: Compressing Strings
 // Write an algorithm that takes a string with repeated characters and compresses them, using a number to show how many times the repeated character has been compressed. For instance, aaa would be written as 3a. Solve the problem with and without recursion.
 
